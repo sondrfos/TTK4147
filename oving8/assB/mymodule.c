3673c6a7f1ad5d97fdcc0d2bbab3f7c0aaacf4be
student <student@Sanntid.(none)>
2017-10-24 10:43:14 +0200

    It's fine

--- oving8/assB/mymodule.c
+++ oving8/assB/mymodule.c
@@ -1,10 +1,6 @@
-/*  
- *  hello-1.c - The simplest kernel module.
- */
- 
 #include <linux/module.h>	/* Needed by all modules */
 #include <linux/kernel.h>	/* Needed for KERN_INFO */
-#include <linux/proc_fs.h>  /* 'Det er proc' Sondre Foslien 20171017*/
+#include <linux/proc_fs.h> 
 
 
 int procfile_read(char *buffer, char **buffer_location, off_t offset, int buffer_length, int *eof, void *data){
@@ -12,12 +8,12 @@ int procfile_read(char *buffer, char **buffer_location, off_t offset, int buffer
 		return 0;
 	}
 	else{
-		return sprintf(buffer, "Hello world\n");
+		return sprintf(buffer, "Hello proc\n");
 	}
 }
 
 int init_module(void){
-	printk(KERN_INFO "Hello world 1.\n");
+	printk(KERN_INFO "Hello world\n");
 
 	struct proc_dir_entry* proc_entry = create_proc_entry("myproc",0644,NULL);
 	proc_entry->read_proc = procfile_read;
@@ -26,6 +22,6 @@ int init_module(void){
 }
 
 void cleanup_module(void){
-	printk(KERN_INFO "Goodbye world 1.\n");
+	printk(KERN_INFO "Goodbye world\n");
 	remove_proc_entry("myproc", NULL);
 }
